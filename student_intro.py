c5d6c1238f6974ee8f7ad6fd2ffed3290aca3319
robertlowder <rlowder@confederationcollege.ca>
2025-01-20 11:06:16 -0500

    Created student_intro.py
    Prints out my name, occupation and very brief bio.

---
+++ student_intro.py
@@ -0,0 +1,17 @@
+"""
+The Student Introduction Program
+
+This is a program that prints out my name, occupation and brief biography.
+To use it, run `student_intro.py`.
+"""
+
+# This where it will print my name
+print('Name: Robert Lowder')
+
+# This is where it will print my Occupation
+# Normally I wouldn't waste energy over commenting such simple code
+print('Occupation: Telecommunications Operations Technician')
+
+print('Background: I was born in a place that was not very interesting. \n'
+      'Luckily I moved away from there and lived somewhere else \n'
+      'However I ended up moving back there and here I am...')
